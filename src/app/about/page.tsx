30a427f69815b8e9b06785504ce7ddc4e64da066
jannatulFerdeous <angeljannat673@gmail.com>
2024-09-23 12:52:51 +0600

    added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
+import About from "@modules/AboutUs/About";
+
 export default function AboutPage() {
   return (
     <>
-      <div className="min-h-[100vh] flex justify-center items-center">
-        <p className="text-3xl">This is about page</p>
-      </div>
+      <About/>
     </>
   );
 }
--- src/common/components/GetTouch/GetTouch.tsx
+++ src/common/components/GetTouch/GetTouch.tsx
@@ -5,16 +5,17 @@ const GetTouch = () => {
   const { title, description, button } = GetTouch_data;
 
   return (
-    <div className="container bg-blue-50 py-14  w-full items-center mt-8 ">
-      <div className="grid grid-cols-2 mx-24 items-center gap-28  ">
-        <div className="ml-44">
+    <div className="container bg-blue-50 py-14 w-full mt-8">
+   
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 items-center gap-8 lg:gap-28 mx-6 sm:mx-12 md:mx-16 lg:mx-24">
+        <div className="md:ml-10 lg:ml-24 text-center md:text-left">
           <h2 className="text-sm font-semibold text-gray-600 mb-2">{title}</h2>
-          <p className="text-2xl md:text-3xl text-gray-900 mt-4">
+          <p className="text-xl sm:text-3xl md:text-4xl text-gray-900 mt-4">
             {description}
           </p>
         </div>
-        <div className=" text-center">
-          <button className="bg-blue-900  hover:bg-primary text-white px-7 py-4  font-medium">
+        <div className="text-center">
+          <button className="bg-blue-900 hover:bg-primary text-white px-7 py-4 font-medium">
             {button.text}
           </button>
         </div>
